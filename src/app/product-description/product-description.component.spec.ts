be660c3c1095ecf74515764721012d2522d7969e
carlosge10 <carlosge10@hotmail.com>
2020-10-30 17:41:07 -0600

    Product Description Component

---
+++ src/app/product-description/product-description.component.spec.ts
@@ -0,0 +1,20 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProductDescriptionComponent } from './product-description.component';
+
+describe('ProductPageComponent', () => {
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+
+  });
+
+  it('should create', () => {
+  });
+});
