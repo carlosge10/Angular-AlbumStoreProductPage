223b2996a399209d42d35b4453f5912526c567fb
carlosge10 <carlosge10@hotmail.com>
2020-11-02 17:21:25 -0600

    Adding Tracklisting Component

---
+++ src/app/product-tracklisting/product-tracklisting.component.spec.ts
@@ -0,0 +1,20 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProductDescriptionComponent } from './product-tracklisting.component';
+
+describe('ProductPageComponent', () => {
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+
+  });
+
+  it('should create', () => {
+  });
+});
