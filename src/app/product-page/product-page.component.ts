54bb069c2a583f72ee4f6756fadcd22a04291c3d
carlosge10 <carlosge10@hotmail.com>
2020-11-02 12:20:09 -0600

    creating a service to get album data

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductDescriptionComponent } from '../product-description/product-description.component';
 
 @Component({
   selector: 'app-product-page',
